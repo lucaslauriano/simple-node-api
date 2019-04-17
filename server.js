11875e100d0e168bb95512022ffc30c0c5dee5de
Lucas Lauriano de Souza <lucaslauriano@live.com>
2019-04-17 07:32:50 -0300

    Server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 //Iniciando o App
 const app = express();
@@ -9,13 +9,13 @@ app.use(express.json());
 app.use(cors());
 
 //Iniciando o DB
-mongoose.connect(
-    'mongodb://localhost:27017/nodeapi',
-    { useNewUrlParser: true }
-);
-requireDir('./src/models');
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true
+});
+
+requireDir("./src/models");
 
 // Routes
-app.use('/api', require('./src/routes'));
+app.use("/api", require("./src/routes"));
 
 app.listen(3000);
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Product = mongoose .model('Product');
+const Product = mongoose.model('Product');
 
  module.exports = { 
      async index(req, res) {
